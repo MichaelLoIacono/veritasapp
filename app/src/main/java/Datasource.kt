914365db8d5d7e141bc5f0e3500af191ad41934d
Shishamicha <fragmentox88@gmail.com>
2022-07-12 17:16:45 +0200

    Datasource versuch2

--- app/src/main/java/Datasource.kt
+++ app/src/main/java/Datasource.kt
@@ -1,22 +1,88 @@
 package com.example.poste
 
+import android.content.Context
 import de.syntaxinstitut.veritasapp.R
 
-class Datasource {
-    fun loadposte() :List<Poste> {
-        return listOf(
-            Poste("Yugi", R.drawable.bild1),
-            Poste("Tobi", R.drawable.bild2),
-            Poste("Kakashi", R.drawable.bild3),
-            Poste("Augilera", R.drawable.bild4),
-            Poste("Deku", R.drawable.bild5),
-            Poste("Son Goku", R.drawable.bild6),
-            Poste("Green Ranger", R.drawable.bild7),
-            Poste("Spider Man", R.drawable.bild8),
-            Poste("Camoran", R.drawable.bild9),
 
+
+/**
+ * Diese Klasse bereitet Daten aus den Ressourcen auf
+ */
+class Datasource(private val context: Context) {
+
+    private val numberOfTitles = 9 // anpassen, wenn Anzahl an Bildtiteln geändert wurde
+
+    /**
+     * Diese Funktion holt die Titel & Bilder aus der Quelldatei
+     * und liefert eine Liste aus Filmen zurück
+     */
+    fun loadMovies(): List<Poste> {
+        // Die Liste an Bilder
+        val postes = mutableListOf<Poste>()
+
+        // Befülle die Liste
+        for (index in 1..numberOfTitles) {
+
+            // Hole den Titel und das Bild
+            val title = getTitle(index)
+            val image = getImage(title)
+            val screen = getScreen(title)
+
+            // Füge ein Movie Objekt hinzu
+            postes.add(
+
+                Poste(title, image,screen)
+                       // Poste(title, image, screen)
 
             )
+        }
+        return postes
+    }
 
+
+
+    /**
+     * Diese Funktion liefert den Titel Nummer index aus der Quelle
+     */
+    private fun getTitle(index: Int): Int {
+        return context.resources.getIdentifier(
+            "posteTitle$index",
+            "string",
+            context.packageName
+        )
+    }
+    /**
+     * Diese Funktion liefert zu jedem übergebenen Titel das entsprechende Bild
+     */
+    private fun getImage(titleRes: Int): Int {
+
+        // Hole den Titel aus den Ressourcen
+        val title = context.getString(titleRes).lowercase()
+            .replace(" ", "_")
+            .replace("ü", "_")
+            .replace("ö", "_")
+
+        // Liefere das entsprechende Bild aus der Quelle
+        return context.resources.getIdentifier(
+            title,
+            "drawable",
+            context.packageName
+        )
+    }
+
+    private fun getScreen(titleRes: Int): Int {
+
+        // Hole den Titel aus den Ressourcen
+        val title = context.getString(titleRes).lowercase()
+            .replace(" ", "_")
+            .replace("ü", "_")
+            .replace("ö", "_")
+
+        // Liefere das entsprechende Bild aus der Quelle
+        return context.resources.getIdentifier(
+            title+"_screen",
+            "drawable",
+            context.packageName
+        )
     }
 }
